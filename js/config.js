bcab6a380ee0fd5c6bef832c5e15d5830e0a43e9
TreJohnson1 <48895037+TreJohnson1@users.noreply.github.com>
2021-02-06 00:35:52 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_NRhFxO153', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'b9ukpbeq96u1dog26ucvaotac', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
